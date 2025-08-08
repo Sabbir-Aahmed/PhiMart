285a94ed9455bc529f7699804eea88753aa05d39
nishi <sharminnishi2424@gmail.com>
2025-08-09 00:08:30 +0600

    order cancel action added

--- order/serializers.py
+++ order/serializers.py
@@ -6,6 +6,9 @@ from order.models import Order,OrderItem
 from order.services import OrderService
 
 
+class EmptySerializer(serializers.Serializer):
+    pass
+
 class SimpleProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -126,19 +129,19 @@ class UpdateOrderSerializer(serializers.ModelSerializer):
             'status'
         ]
 
-    def update(self, instance, validated_data):
-        user = self.context['user']
-        new_status = validated_data['status']
+    # def update(self, instance, validated_data):
+    #     user = self.context['user']
+    #     new_status = validated_data['status']
 
-        if new_status == Order.CANCELED:
-            return OrderService.cancel_order(order=instance, user=user)
+    #     if new_status == Order.CANCELED:
+    #         return OrderService.cancel_order(order=instance, user=user)
 
-        if not user.is_staff :
-            raise serializers.ValidationError(
-                {'detail': 'You are not allowd to update this order'}
-            )
+    #     if not user.is_staff :
+    #         raise serializers.ValidationError(
+    #             {'detail': 'You are not allowd to update this order'}
+    #         )
         
-        return super().update(instance, validated_data)
+    #     return super().update(instance, validated_data)
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemsSerializer(many=True)
--- order/views.py
+++ order/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from order.models import Cart,CartItem,Order,OrderItem
-from order.serializers import CartSerializer,CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer,OrderSerializer,CreateOrderSerializer,UpdateOrderSerializer
+from order.serializers import CartSerializer,CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer,OrderSerializer,CreateOrderSerializer,UpdateOrderSerializer,EmptySerializer
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.exceptions import ValidationError
+from rest_framework.decorators import action
+from order.services import OrderService
+from rest_framework.response import Response
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     serializer_class = CartSerializer
@@ -41,12 +44,20 @@ class CartItemViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     http_method_names = ['get','delete','patch', 'post', 'head', 'options']
     
+    @action(detail=True, methods=['post'], permission_classes = [IsAuthenticated])
+    def cancel(self,request, pk=None):
+        order = self.get_object()
+        OrderService.cancel_order(order=order, user = request.user)
+        return Response({'status': 'Order Canceled'})
+
     def get_permissions(self):
         if self.request.method == 'DELETE':
             return [IsAdminUser()]
         return [IsAuthenticated()]
 
     def get_serializer_class(self):
+        if self.action == 'cancel':
+            return EmptySerializer
         if self.request.method == 'POST':
             return CreateOrderSerializer
         elif self.request.method == 'PATCH':
