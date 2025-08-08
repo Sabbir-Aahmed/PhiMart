9b8bdbccc03f41924a9142960c0b86bd43e45759
nishi <sharminnishi2424@gmail.com>
2025-08-07 21:23:12 -0700

    authentication applied

--- order/views.py
+++ order/views.py
@@ -13,14 +13,14 @@ class CartItemViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return AddCartItemSerializer
-        
+      
         elif self.request.method == 'PATCH':
             return UpdateCartItemSerializer
         return CartItemSerializer
-    
+
     def get_serializer_context(self):
         return {'cart_id': self.kwargs['cart_pk']}
-    
+  
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id = self.kwargs['cart_pk'])
--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'api',
     'users',
     'product',
@@ -132,4 +134,23 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+   "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+DJOSER = {
+    "SERIALIZERS": {
+        "user_create": "users.serializers.UserCreateSerializer",
+        'currest_user': 'users.serializers.UserSerializer',
+        
+    }
 }
