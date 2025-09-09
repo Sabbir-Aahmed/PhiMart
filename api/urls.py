c7814193c76f99425af28ff9b62d4801622281da
nishi <sharminnishi2424@gmail.com>
2025-09-10 01:43:12 +0600

    payment succes/fail/cancel added

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from product.views import ProductViewSets, CategoryViewSets, ReviewViewSet, ProductImageViewSet
 from rest_framework_nested import routers
-from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment
+from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment, payment_success, payment_cancel,payment_fail
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSets , basename='products')
@@ -22,5 +22,8 @@ urlpatterns = [
     path('', include(cart_router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('payment/initiate/',initiate_payment, name="initiate_payment" )
+    path('payment/initiate/',initiate_payment, name="initiate_payment" ),
+    path('payment/success/',payment_success, name="payment_success" ),
+    path('payment/fail/',payment_fail, name="payment_fail" ),
+    path('payment/cancel/',payment_cancel, name="payment_cancel" ),
 ]
