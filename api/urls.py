7eb9a86985ae1e93be22c08bd77c32105e33fe28
nishi <sharminnishi2424@gmail.com>
2025-09-09 19:06:44 +0600

    Initiating Payment API

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from product.views import ProductViewSets, CategoryViewSets, ReviewViewSet, ProductImageViewSet
 from rest_framework_nested import routers
-from order.views import CartViewSet, CartItemViewSet, OrderViewSet
+from order.views import CartViewSet, CartItemViewSet, OrderViewSet, initiate_payment
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSets , basename='products')
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include(cart_router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('payment/initiate/',initiate_payment, name="initiate_payment" )
 ]
